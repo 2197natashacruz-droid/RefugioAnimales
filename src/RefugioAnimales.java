65cfaac73e6e2bae80e79a9820922d8f40897e34
Kevin Lizama <kevinp.lizama@gmail.com>
2026-04-09 15:05:21 -0400

    feat: Creacion class Refugio de animales

---
+++ src/RefugioAnimales.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class RefugioAnimales {
+    public static void main(String[] args) {
+        //se llama la utilidad scanner
+        Scanner scan = new Scanner(System.in);
+        //aquí declaramos e iniciamos variables
+        boolean activo = true;
+        List<String> animales = new ArrayList<>();
+        Map<String, String> estadoAnimal = new HashMap<>();
+        Set<String> especies = new HashSet<>();
+        Map<String, String> animalEspecie = new HashMap<>();
+        String[] estados = {"Disponible","Adoptado"};
+
+
+    }
+}
